5ef69e3f0dff50030478e9665c2095b4bf715f32
Arttu mahlakaarto <arttu.mahlakaarto@gmail.com>
2023-03-02 07:29:44 +0200

    Disabled the PAIGE popups

--- PaigeBGoneProject/PaigeBGone/PaigeBGoneMod.cs
+++ PaigeBGoneProject/PaigeBGone/PaigeBGoneMod.cs
@@ -1,6 +1,10 @@
 using SpaceWarp.API.Mods;
 using KSP.Game;
-
+using HarmonyLib;
+using KSP.UI;
+using UnityEngine;
+using KSP.Game.Missions.Definitions;
+using System.Collections.Generic;
 namespace PaigeBGone
 {
     [MainMod]
@@ -10,6 +14,21 @@ namespace PaigeBGone
         {
             PersistentProfileManager.VoiceVolume = 0f;
             PersistentProfileManager.IsFTUEEnabled = false;
+
+            Logger.Info("[PAIGEBGONE]");
+        }
+        [HarmonyPatch(typeof(KSP.Game.Missions.KSP2MissionManager))]
+        [HarmonyPatch("ApplyProgressFromSavedGame")]
+        public class ApplyProgressFromSavedGamePatch
+        {
+            static void Postfix()
+            {
+                GameInstance a = GameManager.Instance.Game;
+                var Undesirables = new List<MissionType> () { MissionType.Tutorial, MissionType.FTUE };
+                var MissionList = a.KSP2MissionManager.Missions;
+                MissionList.Find((MissionData x) =>  Undesirables.Contains(x.type)).state = KSP.Game.Missions.State.MissionState.Complete;
+                Debug.Log("[PAIGEBGONE] Muted P.A.I.G.E forever");
+            }
         }
     }
 }
